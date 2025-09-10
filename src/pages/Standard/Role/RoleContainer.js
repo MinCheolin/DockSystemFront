d2c8ae5b21904143c757ef7356d78ebf56196f13
unknown <windn0503@gmail.com>
2025-09-10 15:05:27 +0900

    Role CRUD func add

--- src/pages/Standard/Role/RoleContainer.js
+++ src/pages/Standard/Role/RoleContainer.js
@@ -1,14 +1,56 @@
 import RolePresenter from "./RolePresenter";
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import axios from 'axios';
 
 const RoleContainer = () =>{
-     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [roleInfo, setRoleInfo] = useState({
+  const [roles, setRoles] = useState([]); 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [updateRoleInfo,setUpdateRoleInfo] = useState({
+    roleNo:"",    
+    roleName:""
+    });
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+  const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+  const [roleInfo, setRoleInfo] = useState({
         roleName:""
     });
 
-     const onChangeInputHandler= (e) => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/api/erp/v1/roles');
+        setRoles(response.data);    
+      } catch (err) {
+        
+      } finally {
+      }
+    };
+    useEffect(() => {
+      fetchData();
+    }, []); 
+
+ const rowSelection = {
+    selectedRowKeys,
+    hideSelectAll: true,
+    onChange: (selectedRowKeys) => {
+      const lastKey = selectedRowKeys.pop();
+      setSelectedRowKeys(lastKey ? [lastKey] : []);
+    },
+    
+  };
+
+  const HandleRowClick = (record) => {
+    if (selectedRowKeys.includes(record.roleNo)) {
+      setSelectedRowKeys([]);      
+    } else {
+      setSelectedRowKeys([record.roleNo]); 
+    }
+  };
+
+
+
+  const hasSelected = selectedRowKeys.length > 0;
+
+  const HandleChangeInput= (e) => {
         const { name, value } = e.target;
         setRoleInfo(prev => ({
         ...prev,
@@ -16,33 +58,80 @@ const RoleContainer = () =>{
         }));
     };
 
-    const onChange = (pagination, filters, sorter, extra) => {
-        console.log('params', pagination, filters, sorter, extra);
-    };
+ const HandleDoubleClick = (record) => {
+    setSelectedRowKeys([record.roleNo]);
+    setUpdateRoleInfo({ ...record });             
+    setIsUpdateModalOpen(true);    
+  };
+
+const HandleUpdateChangeInput = (e) => {
+  const { name, value } = e.target;
+  setUpdateRoleInfo(prev => ({
+    ...prev,
+    [name]: value
+  }));
+};
 
 
-    const CreateRoleHandler = async () => {
+
+  const HandleCreateRole = async () => {
        try {
-             console.log("폼 데이터 :",roleInfo);
-            const response = await axios.post('http://localhost:8080/api/erp/v1/roles', roleInfo);
-            console.log('등록 성공:', response.data);
+          const response = await axios.post('http://localhost:8080/api/erp/v1/roles', roleInfo);
+          console.log('등록 성공:', response.data);
         } catch (error) {
         console.error('등록 실패:', error);
         }
     setIsModalOpen(false);
   }
+  const HandleDeleteRole = async () =>{
+    if (selectedRowKeys.length === 0) return;  
+    console.log(selectedRowKeys[0]);
+   try {
+    await axios.delete(`http://localhost:8080/api/erp/v1/roles/${selectedRowKeys[0]}`);
+    setRoles(prev => prev.filter(item => item.roleNo !== selectedRowKeys[0]));
+    setSelectedRowKeys([]);
+  } catch (err) {
+    console.error(err);
+    alert('삭제 실패');
+  }
+       fetchData();
+  }
 
-  const handleAddClick = () => {
+  const HandleUpdateRole = async () =>{
+        const finalData = {
+            ...updateRoleInfo,
+            roleNo: Number(updateRoleInfo.roleNo),        
+        };
+       try {
+          await axios.put(`http://localhost:8080/api/erp/v1/roles/${finalData.roleNo}`,finalData);
+        } catch (err) {
+           console.error(err);
+           alert('수정 실패');
+        }
+        setIsUpdateModalOpen(false);
+         fetchData();
+  }
+
+  const HandleCreateModalOpen = () => {
+    setRoleInfo('');
     setIsModalOpen(true);
   };
 
-  const handleModalClose = () => {
+  const HandleModalClose = () => {
     setIsModalOpen(false);
   };
-    return (
-        <RolePresenter onChangeInputHandler={onChangeInputHandler} onChange={onChange} CreateRoleHandler={CreateRoleHandler}
-                handleAddClick={handleAddClick} handleModalClose={handleModalClose} isModalOpen={isModalOpen}/>
-    );
+
+    const HandleUpdateModalClose = () => {
+    setIsUpdateModalOpen(false);
+  };
+
+  return (
+        <RolePresenter HandleChangeInput={HandleChangeInput} HandleCreateRole={HandleCreateRole} HandleDeleteRole={HandleDeleteRole} setIsUpdateModalOpen={setIsUpdateModalOpen}
+            updateRoleInfo={updateRoleInfo}       HandleUpdateChangeInput ={HandleUpdateChangeInput} HandleUpdateRole={HandleUpdateRole}
+        HandleCreateModalOpen={HandleCreateModalOpen} HandleModalClose={HandleModalClose} isModalOpen={isModalOpen} isUpdateModalOpen={isUpdateModalOpen}roles={roles}
+                rowSelection={rowSelection} HandleRowClick={HandleRowClick} hasSelected={hasSelected} HandleDoubleClick={HandleDoubleClick} HandleUpdateModalClose={HandleUpdateModalClose}/>
+               
+              );  
 }
 
 export default RoleContainer;
--- src/pages/Standard/Role/RolePresenter.js
+++ src/pages/Standard/Role/RolePresenter.js
@@ -1,62 +1,99 @@
-import {useState} from "react";
 import { Button, Table, Modal, Form, Input} from 'antd';
 import "./role.css";
 
 
-const RolePresenter = ({onChangeInputHandler ,onChange, CreateRoleHandler,handleAddClick,handleModalClose,isModalOpen}) =>{
-    const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-    const [loading, setLoading] = useState(false);
-  
-  const onSelectChange = newSelectedRowKeys => {
-    console.log('selectedRowKeys changed: ', newSelectedRowKeys);
-    setSelectedRowKeys(newSelectedRowKeys);
-  };
-  const rowSelection = {
-    selectedRowKeys,
-    onChange: onSelectChange,
-  };
-  const hasSelected = selectedRowKeys.length > 0;
-  
+const RolePresenter = ({roles,HandleChangeInput , HandleCreateRole,HandleCreateModalOpen,HandleModalClose,isModalOpen,HandleUpdateModalClose,
+                          HandleUpdateRole,rowSelection, HandleRowClick, hasSelected,HandleDeleteRole, HandleDoubleClick,isUpdateModalOpen,updateRoleInfo,HandleUpdateChangeInput }) =>{
+ const columns = [
+   {
+      title: '번호',
+      key: 'index',
+      render: (text, record, index) => index + 1, 
+    },
+
+  {
+    title: '직급명',
+    dataIndex: 'roleName',
+    key: 'name',
+  }
+  ];
+
   return(
     <>
         <div className="role-content">
             <div className="grid-func">
-                <div></div>
+                <div className="role-list">직급 목록</div>
                 <div className="func-button">
-                   <Button onClick={handleAddClick}>추가</Button>
-                   <Button type="primary" danger >삭제</Button>                
+                   <Button onClick={HandleCreateModalOpen}>추가</Button>
+                   <Button type="primary" danger  disabled={!hasSelected} onClick={HandleDeleteRole}>삭제</Button>                
                 </div>
             </div>
             <div className="grid-box">
                   <Table size="small"
-                   rowSelection={rowSelection}
-                   onChange={onChange}
                    pagination={false}
-                   rowClassName="clickable-row"/>
+                   rowClassName="clickable-row"
+                   rowSelection={rowSelection}
+                   columns={columns} dataSource={roles} 
+                   rowKey="roleNo" 
+                   onRow={(record) => ({
+                       onClick: () => HandleRowClick(record),
+                       onDoubleClick: () => { HandleDoubleClick(record);
+                       } 
+                    })}
+                   />     
         </div>
+    
      <Modal
-        title="부서 추가"
+        title="직급 추가"
         open={isModalOpen}
         footer={null}   
+        onCancel={HandleModalClose} 
       >
 
-     <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={CreateRoleHandler}>
-      <Form.Item label="직급명">   <Input placeholder="직급명을 입력하세요." name="roleId" onChange={onChangeInputHandler} /> </Form.Item>
-      
-     
-      <Form.Item> 
-      <Button type="primary" htmlType="submit">
+     <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={HandleCreateRole}>
+      <Form.Item label="직급명">   <Input placeholder="직급명을 입력하세요." name="roleName" onChange={HandleChangeInput} /> </Form.Item>
+      <Form.Item wrapperCol={{ span: 24 }}> 
+        <div className="modal-form-button">      <Button type="primary" htmlType="submit">
         추가
       </Button>
-      <Button style={{ marginLeft: 8 }} onClick={handleModalClose}>
+      <Button onClick={HandleModalClose}>
         닫기
       </Button>
+      </div>
     </Form.Item>
-     
      </Form>
-      
       </Modal>
-        </div>
+
+    <Modal
+        title="직급 수정"
+        open={isUpdateModalOpen}
+        footer={null}   
+        onCancel={HandleUpdateModalClose} 
+      >
+
+     <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={HandleUpdateRole}>
+      <Form.Item label="직급명">   <Input placeholder="직급명을 입력하세요." name="roleName"
+       value={updateRoleInfo.roleName}  // 상태 반영
+      onChange={HandleUpdateChangeInput}
+      //onChange={HandleChangeInput}
+       /> </Form.Item>
+      <Form.Item wrapperCol={{ span: 24 }}> 
+        <div className="modal-form-button">      <Button type="primary" htmlType="submit">
+        수정
+      </Button>
+      <Button onClick={HandleUpdateModalClose}>
+        닫기
+      </Button>
+      </div>
+    </Form.Item>
+     </Form>
+      </Modal>
+    
+
+
+
+
+     </div>
         </>
     );
 }
