9aa2ff76841ae79e42866a08a308284759e34862
unknown <windn0503@gmail.com>
2025-09-11 14:14:30 +0900

    user, department, role crud create

--- src/pages/Standard/Role/RolePresenter.js
+++ src/pages/Standard/Role/RolePresenter.js
@@ -2,8 +2,14 @@ import { Button, Table, Modal, Form, Input} from 'antd';
 import "./role.css";
 
 
-const RolePresenter = ({roles,HandleChangeInput , HandleCreateRole,HandleCreateModalOpen,HandleModalClose,isModalOpen,HandleUpdateModalClose,
-                          HandleUpdateRole,rowSelection, HandleRowClick, hasSelected,HandleDeleteRole, HandleDoubleClick,isUpdateModalOpen,updateRoleInfo,HandleUpdateChangeInput }) =>{
+const RolePresenter = ({  rowSelection, hasSelected,roles,updateRoleInfo,isModalOpen,isUpdateModalOpen,
+                          HandleRowClick,HandleDoubleClick,
+                          HandleCreateRole, HandleUpdateRole,HandleDeleteRole,
+                          HandleChangeInput,HandleUpdateChangeInput,
+                          HandleCreateModalOpen,HandleModalClose,HandleUpdateModalClose
+                        }) =>{
+                           
+ const [form] = Form.useForm();
  const columns = [
    {
       title: '번호',
@@ -25,13 +31,12 @@ const RolePresenter = ({roles,HandleChangeInput , HandleCreateRole,HandleCreateM
                 <div className="role-list">직급 목록</div>
                 <div className="func-button">
                    <Button onClick={HandleCreateModalOpen}>추가</Button>
-                   <Button type="primary" danger  disabled={!hasSelected} onClick={HandleDeleteRole}>삭제</Button>                
+                   <Button type="primary" danger disabled={!hasSelected} onClick={HandleDeleteRole}>삭제</Button>                
                 </div>
             </div>
             <div className="grid-box">
                   <Table size="small"
                    pagination={false}
-                   rowClassName="clickable-row"
                    rowSelection={rowSelection}
                    columns={columns} dataSource={roles} 
                    rowKey="roleNo" 
@@ -48,34 +53,28 @@ const RolePresenter = ({roles,HandleChangeInput , HandleCreateRole,HandleCreateM
         open={isModalOpen}
         footer={null}   
         onCancel={HandleModalClose} 
+        afterClose={() => form.resetFields()}
       >
 
-     <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={HandleCreateRole}>
-      <Form.Item label="직급명">   <Input placeholder="직급명을 입력하세요." name="roleName" onChange={HandleChangeInput} /> </Form.Item>
+     <Form form={form} labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={HandleCreateRole}>
+     <Form.Item label="직급명" name="roleName">   <Input placeholder="직급명을 입력하세요." name="roleName" onChange={HandleChangeInput}/> </Form.Item>
       <Form.Item wrapperCol={{ span: 24 }}> 
-        <div className="modal-form-button">      <Button type="primary" htmlType="submit">
-        추가
-      </Button>
-      <Button onClick={HandleModalClose}>
-        닫기
-      </Button>
-      </div>
-    </Form.Item>
-     </Form>
-      </Modal>
+        <div className="modal-form-button"> 
+          <Button type="primary" htmlType="submit">   추가 </Button>
+          <Button onClick={HandleModalClose}>         닫기 </Button>      </div>
+          </Form.Item> </Form> </Modal>
 
     <Modal
         title="직급 수정"
         open={isUpdateModalOpen}
         footer={null}   
-        onCancel={HandleUpdateModalClose} 
+        onCancel={HandleUpdateModalClose}
       >
 
      <Form labelCol={{ span: 4 }} wrapperCol={{ span: 20 }} onFinish={HandleUpdateRole}>
       <Form.Item label="직급명">   <Input placeholder="직급명을 입력하세요." name="roleName"
-       value={updateRoleInfo.roleName}  // 상태 반영
+       value={updateRoleInfo.roleName} 
       onChange={HandleUpdateChangeInput}
-      //onChange={HandleChangeInput}
        /> </Form.Item>
       <Form.Item wrapperCol={{ span: 24 }}> 
         <div className="modal-form-button">      <Button type="primary" htmlType="submit">
